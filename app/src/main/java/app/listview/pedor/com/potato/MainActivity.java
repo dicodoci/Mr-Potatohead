65f9f4b323c4a3a5d3aea2758fe6e28355617e8a
Dico <dico.vanleeuwen@student.uva.nl>
2018-02-08 21:36:47 +0100

    Removed title bar; Added comments

--- app/src/main/java/app/listview/pedor/com/potato/MainActivity.java
+++ app/src/main/java/app/listview/pedor/com/potato/MainActivity.java
@@ -13,32 +13,38 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-
+//  Create Hashmap for keeping track of visibilities.
     public Map<String, Integer> visibility = new HashMap<String, Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Map<String, Integer> visibility = new HashMap<String, Integer>();
-        String[] myints = {"arms", "ears", "eyebrows", "eyes", "glasses", "hat", "mouth", "mustache", "nose", "shoes"} ;
+        String[] myints = {"arms", "ears", "eyebrows", "eyes", "glasses", "hat", "mouth",
+                           "mustache", "nose", "shoes"} ;
+
+//      If there is a previous state, put state in the hashmap, else start with invisible.
         for(String part : myints) {
             if (savedInstanceState != null) {
                 visibility.put(part, savedInstanceState.getInt(part));
             } else {
                 visibility.put(part, View.INVISIBLE);
             }
-            System.out.println(visibility.get("eyes"));
-            ImageView image = (ImageView) findViewById(getResources().getIdentifier(part, "id", getPackageName()));
+//          Get identifier of an image by name.
+            ImageView image = (ImageView) findViewById(getResources().getIdentifier(part,
+                    "id", getPackageName()));
             image.setVisibility(visibility.get(part));
         }
     }
 
-
+//  Change visibility based on checkbox.
     public void checkClicked(View v) {
         Log.d("potato", "checkClicked: ");
         CheckBox checkbox = (CheckBox) v;
         String bodyPart = checkbox.getText().toString().toLowerCase();
-        ImageView image = (ImageView) findViewById(getResources().getIdentifier(bodyPart, "id", getPackageName()));
+//      Get identifier of an image by name.
+        ImageView image = (ImageView) findViewById(getResources().getIdentifier(bodyPart,
+                "id", getPackageName()));
         if (checkbox.isChecked()) {
             image.setVisibility(View.VISIBLE);
             visibility.put(bodyPart, View.VISIBLE);
@@ -48,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+//  Save the current state.
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState); // always call super
         for (Map.Entry<String, Integer> combination : visibility.entrySet()) {
